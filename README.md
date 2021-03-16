5637252173d83f1af070a96c5a6938f5c98d7dd8
Florian Aumeier <f.aumeier@me.com>
2021-03-16 10:21:01 +0100

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+<h1 align="center">Vitesse Owl Theme for VS Code</h1>
+
+## Thanks
+
+This project is based on [night-owl-vscode-theme](https://github.com/sdras/night-owl-vscode-theme) and [vscode-theme-vitesse](https://github.com/antfu/vscode-theme-vitesse).
---
+++ src/colors.ts
@@ -0,0 +1,53 @@
+export const colors = {
+  black: '#111',
+  white: '#fff',
+  gray: ['#fafbfc', '#f6f8fa', '#e1e4e8', '#d1d5da', '#959da5', '#6a737d', '#586069', '#444d56', '#2f363d', '#24292e'],
+  blue: ['#f1f8ff', '#dbedff', '#c8e1ff', '#79b8ff', '#2188ff', '#0366d6', '#005cc5', '#044289', '#032f62', '#05264c'],
+  green: ['#f0fff4', '#dcffe4', '#bef5cb', '#85e89d', '#34d058', '#28a745', '#22863a', '#176f2c', '#165c26', '#144620'],
+  yellow: ['#fffdef', '#fffbdd', '#fff5b1', '#ffea7f', '#ffdf5d', '#ffd33d', '#f9c513', '#dbab09', '#b08800', '#735c0f'],
+  orange: ['#fff8f2', '#ffebda', '#ffd1ac', '#ffab70', '#fb8532', '#f66a0a', '#e36209', '#d15704', '#c24e00', '#a04100'],
+  red: ['#ffeef0', '#ffdce0', '#fdaeb7', '#f97583', '#ea4a5a', '#d73a49', '#cb2431', '#b31d28', '#9e1c23', '#86181d'],
+  purple: ['#f5f0ff', '#e6dcfd', '#d1bcf9', '#b392f0', '#8a63d2', '#6f42c1', '#5a32a3', '#4c2889', '#3a1d6e', '#29134e'],
+  pink: ['#ffeef8', '#fedbf0', '#f9b3dd', '#f692ce', '#ec6cb9', '#ea4aaa', '#d03592', '#b93a86', '#99306f', '#6d224f'],
+}
+
+// [dark, light]
+export const VitesseThemes = {
+  primary: ['#4d9375', '#1c6b48'],
+
+  foreground: ['#dbd7ca', '#393a34'],
+  activeForeground: ['#bfbaaa', '#4e4f47'],
+  secondaryForeground: ['#dedcd590', '#393a3490'],
+  ignored: ['#dedcd530', '#393a3450'],
+  faded: ['#dedcd510', '#393a3410'],
+  border: ['#181818', '#f0f0f0'],
+  background: ['#121212', '#ffffff'],
+  activeBackground: ['#181818', '#f5f5f5'],
+
+  comment: ['#758575', '#a0ada0'],
+  string: ['#c98a7d', '#b56959'],
+  literal: ['#429988', '#2f8a89'],
+  variable: ['#b8a965', '#8c862b'],
+  keyword: ['#4d9375', '#1c6b48'],
+  number: ['#6394bf', '#296aa3'],
+  boolean: ['#4d9375', '#1c6b48'],
+  function: ['#a1b567', '#6c7834'],
+  constant: ['#d4976c', '#a65e2b'],
+  class: ['#5eaab5', '#2993a3'],
+  interface: ['#67b893', '#2e8f63'],
+  builtin: ['#cb7676', '#ab5959'],
+  property: ['#e0a569', '#b58451'],
+  namespace: ['#db889a', '#b05a78'],
+  punctuation: ['#858585', '#8e8f8b'],
+  decorator: ['#bd8f8f', '#bd8f8f'],
+  regex: ['#c4704f', '#ab5e3f'],
+
+  // colors
+  green: ['#4d9375', '#1c6b48'],
+  cyan: ['#5eaab5', '#2993a3'],
+  blue: ['#6394bf', '#296aa3'],
+  red: ['#cb7676', '#ab5959'],
+  orange: ['#d4976c', '#a65e2b'],
+  yellow: ['#e6cc77', '#bda437'],
+  magenta: ['#db889a', '#b05a78'],
+}
